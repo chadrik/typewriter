575b2a5ed001a939244fffdb688e41014bf254ab
Chad Dombrova <chadrik@gmail.com>
2020-09-27 15:28:46 -0700

    Add support for reading settings from a config file

--- pyannotate_tools/annotations/__main__.py
+++ pyannotate_tools/annotations/__main__.py
@@ -4,23 +4,29 @@ import argparse
 import json
 import logging
 import os
+import re
 import sys
-
 from lib2to3.main import StdoutRefactoringTool
-
+from os.path import expanduser, expandvars
 from typing import Any, Dict, List, Optional
+
+import six.moves.configparser as configparser
 
 from pyannotate_tools.annotations.main import generate_annotations_json_string, unify_type_comments
 from pyannotate_tools.fixes.base import BaseFixAnnotateFromSignature
 from pyannotate_tools.fixes.fix_annotate_any import FixAnnotateAny
-from pyannotate_tools.fixes.fix_annotate_json import FixAnnotateJson
 from pyannotate_tools.fixes.fix_annotate_command import FixAnnotateCommand
+from pyannotate_tools.fixes.fix_annotate_json import FixAnnotateJson
+
+NAME = 'pyannotate'
+CONFIG_FILES = [NAME + '.ini', 'setup.cfg']
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('files', nargs='*', metavar="FILE",
                     help="Files and directories to update with annotations")
 
+# json --
 json_group = parser.add_argument_group('json file options',
                                        "Read type info from a json file")
 json_group.add_argument('--type-info', metavar="FILE",
@@ -36,16 +42,19 @@ json_group.add_argument('--uses-signature', action='store_true',
 json_group.add_argument('-s', '--only-simple', action='store_true',
                         help="Only annotate functions with trivial types")
 
+# command --
 cmd_group = parser.add_argument_group('command options',
                                       "Generate type info by calling an "
                                       "external program")
 cmd_group.add_argument('--command', '-c', metavar="COMMAND",
                        help="Command to generate JSON info for a call site")
 
+# any --
 any_group = parser.add_argument_group('any options')
 any_group.add_argument('-a', '--auto-any', action='store_true',
                        help="Annotate everything with 'Any'")
 
+# other --
 other_group = parser.add_argument_group('other options')
 other_group.add_argument('-p', '--print-function', action='store_true',
                          help="Assume print is a function")
@@ -64,6 +73,50 @@ other_group.add_argument('--py2', '-2', action='store_const', dest='python_versi
                          help="Annotate for Python 2 with comments (default)")
 other_group.add_argument('--py3', '-3', action='store_const', dest='python_version', const='3',
                          help="Annotate for Python 3 with argument and return value annotations")
+
+
+def load_config(arg_parser):
+    # type: (argparse.ArgumentParser) -> Dict
+    """Use the ArgumentParser to extract values set in a config file.
+
+    These are used to set the defaults on the parser before calling
+    parse_args(), which ensures that arguments specified on the command line
+    have a higher priority than those set in the config file.
+    """
+    SPLIT = re.compile('[,\n]+')
+    paths = [expanduser(expandvars(path)) for path in CONFIG_FILES]
+    config_parser = configparser.RawConfigParser()
+    config_parser.read(paths)
+    defaults = {}
+    if config_parser.has_section(NAME):
+        for action in arg_parser._actions:
+            if action.dest in defaults:
+                # some arguments may refer to the same destination
+                continue
+            try:
+                if isinstance(action, argparse._HelpAction):
+                    continue
+                elif isinstance(action, (argparse._StoreFalseAction,
+                                         argparse._StoreTrueAction,
+                                         argparse._StoreAction,
+                                         argparse._StoreConstAction)):
+                    if action.type is int:
+                        val = config_parser.getint(NAME, action.dest)
+                    elif action.type is bool:
+                        val = config_parser.getboolean(NAME, action.dest)
+                    elif action.type is float:
+                        val = config_parser.getfloat(NAME, action.dest)
+                    else:
+                        val = config_parser.get(NAME, action.dest)
+                        if action.nargs in {'*', '+'}:
+                            val = [x for x in SPLIT.split(val) if x]
+                else:
+                    raise TypeError(action)
+            except configparser.NoOptionError:
+                continue
+            defaults[action.dest] = val
+
+    return defaults
 
 
 class ModifiedRefactoringTool(StdoutRefactoringTool):
@@ -108,8 +161,11 @@ def dump_annotations(type_info, files):
 def main(args_override=None):
     # type: (Optional[List[str]]) -> None
 
+    defaults = load_config(parser)
+    parser.set_defaults(**defaults)
     # Parse command line.
     args = parser.parse_args(args_override)
+
     if not args.files and not args.dump:
         parser.error("At least one file/directory is required")
 
